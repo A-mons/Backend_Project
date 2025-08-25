202183b902b83d4163cd539134fb3e656e471355
Mons <ah.mounsef@gmail.com>
2025-08-25 23:58:54 +0200

    error handler

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 
 const  app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/subscriptions', subscriptionRouter);
 
-
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
